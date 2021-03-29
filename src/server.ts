4e2ee1acb0605b5b5dfe82ee6d5773f9c5b40281
Benjamin Whitechurch <benjamin.whitechurch@gmail.com>
2021-03-29 17:06:18 +1100

    Add basic firestore emulator config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
 import express from 'express';
 import apiV1 from './api/v1/index';
 
+const initFirestore = () => {
+    const admin = require('firebase-admin');
+
+    const serviceAccount = require('../serviceAccountKey.json');
+
+    admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
 const startServer = () => {
+    initFirestore();
+
     const server = express();
     const hostname = '127.0.0.1';
     const port = 3000;
@@ -14,5 +26,8 @@ const startServer = () => {
         return console.error(error);
     });
 };
+
+// Set any environment variables
+require('dotenv').config();
 
 startServer();
