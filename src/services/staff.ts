4033f9722da0dc3095fbca8a802c730c14c46845
Benjamin Whitechurch <benjamin.whitechurch@gmail.com>
2021-03-30 09:27:54 +1100

    Implement filtering for staff get endpoint

--- src/services/staff.ts
+++ src/services/staff.ts
@@ -5,7 +5,42 @@ import {deleteDocument, getDocument, updateDocument} from './dbHelper';
 
 const getAllStaff = async (filterOptions) => {
     if (filterOptions != null) {
-        // TODO
+        let result = db.collection('staff');
+        // Add filter conditions
+        let filterByAssetType = false;
+        Object.keys(filterOptions).forEach((key) => {
+            if (key == 'assetType') {
+                filterByAssetType = true;
+                // firestore provides no easy way to search inside arrays for
+                // specific key, so do nothing here
+            } else {
+                result = result.where(key, '==', filterOptions[key]);
+            }
+        });
+        result = await result.get();
+
+        result = result.docs.map((staff) => {
+            return {id: staff.id, ...staff.data()};
+        });
+
+        if (filterByAssetType) {
+            const filteredResultsOnType = [];
+            for (const i in result) { // eslint-disable-line guard-for-in
+                // Only add results that match the type filter
+                console.log(result);
+
+                for (const j in result[i].assets) { // eslint-disable-line guard-for-in
+                    if (result[i].assets[j].type == filterOptions.assetType) {
+                        console.log('ass');
+                        filteredResultsOnType.push(result[i]);
+                        break;
+                    }
+                }
+            }
+            result = filteredResultsOnType;
+        }
+
+        return result;
     } else {
         const result = await db.collection('staff').get();
         return result.docs.map((staff) => {
